e2448890d73be55deb6221ca3edd87831bcc105e
Edd Dumbill <edd@usefulinc.com>
2003-12-26 22:21:20 +0000

    added /system/bluetooth/obex-savedir key to specify where beamed files are
    2003-12-26  Edd Dumbill <edd@usefulinc.com>
    
        * obex/servonly.c: added /system/bluetooth/obex-savedir key to specify
        where beamed files are saved.
        * src/permissiondialog.gob: finished dialog, HIGged as well as I can.
        * src/gnomebt-permissiondialog-test.c: exercise perm dialog

---
+++ src/gnomebt-permissiondialog-test.c
@@ -0,0 +1,35 @@
+
+#include <stdlib.h>
+
+#include "gnomebt-permissiondialog.h"
+
+int main(int argc, char **argv)
+{
+    GnomebtController *btctl;
+    GnomebtPermissionDialog *dlg;
+    gint result;
+    
+    gtk_init (&argc, &argv);
+    btctl = gnomebt_controller_new ();
+    dlg = gnomebt_permissiondialog_new (btctl, 
+            "00:80:37:2A:B6:BC", "File transfer requested",
+            "<span weight='bold' size='larger'>Accept a file from '%s'?</span>\n\nThe remote device is attempting to send you a file via Bluetooth.");
+    result = gtk_dialog_run (GTK_DIALOG (dlg));
+    switch (result) {
+        case GTK_RESPONSE_OK:
+            g_message ("user said yes");
+            break;
+        default:
+            g_message ("user said no");
+            break;
+    }
+    if (gnomebt_permissiondialog_remember(dlg)) {
+        g_message ("remember this result");
+    }
+    
+    gtk_widget_destroy(GTK_WIDGET(dlg));
+    g_object_unref(btctl);
+
+    return 0;
+}
+
