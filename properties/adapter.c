b0b4e124cefba8c63ff74b46bd4cc77116914303
Bastien Nocera <hadess@src.gnome.org>
2009-02-27 19:45:11 +0000

    some expand/fill correction on properties dialog
    Patch from  Baptiste Mille-Mathias <bmm80@free.fr> to fix the appearance
    of the properties dialogue when expanded vertically. (Closes: #573464)
    
    svn path=/trunk/; revision=403

--- properties/adapter.c
+++ properties/adapter.c
@@ -435,7 +435,7 @@ static void create_adapter(struct adapter_data *adapter)
 	adapter->child = mainbox;
 
 	vbox = gtk_vbox_new(FALSE, 6);
-	gtk_box_pack_start(GTK_BOX(mainbox), vbox, FALSE, FALSE, 0);
+	gtk_box_pack_start(GTK_BOX(mainbox), vbox, FALSE, TRUE, 0);
 
 	/* The powered checkbox */
 	button = gtk_check_button_new_with_mnemonic (_("_Powered"));
@@ -468,7 +468,7 @@ static void create_adapter(struct adapter_data *adapter)
 
 	/* The friendly name */
 	vbox = gtk_vbox_new(FALSE, 6);
-	gtk_box_pack_start(GTK_BOX(mainbox), vbox, TRUE, TRUE, 0);
+	gtk_box_pack_start(GTK_BOX(mainbox), vbox, FALSE, FALSE, 0);
 
 	label = create_label(_("Friendly name"));
 	gtk_box_pack_start(GTK_BOX(vbox), label, FALSE, FALSE, 0);
@@ -497,7 +497,7 @@ static void create_adapter(struct adapter_data *adapter)
 
 	label = create_label(_("Known devices"));
 	gtk_table_attach(GTK_TABLE(table), label, 0, 2, 0, 1,
-			 GTK_EXPAND | GTK_FILL, GTK_EXPAND | GTK_FILL, 0, 6);
+			 GTK_EXPAND | GTK_FILL, GTK_SHRINK, 0, 6);
 
 	scrolled = gtk_scrolled_window_new(NULL, NULL);
 	gtk_scrolled_window_set_policy(GTK_SCROLLED_WINDOW(scrolled),
