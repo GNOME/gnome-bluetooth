1b43fefcdfbba57c19cab1dae214c3378b454a6d
Ross Burton <ross@linux.intel.com>
2010-04-01 11:02:23 +0100

    Add a banner to the meego panel

--- moblin/main.c
+++ moblin/main.c
@@ -77,7 +77,7 @@ panel_shown_cb (MplPanelClient *client, gpointer user_data)
 {
 	MoblinPanel *panel = MOBLIN_PANEL (user_data);
 
-        moblin_panel_shown (panel);
+	moblin_panel_shown (panel);
 }
 
 int
@@ -85,11 +85,11 @@ main (int argc, char *argv[])
 {
 	MplPanelClient *panel;
 	GtkWidget      *window, *content;
-	GtkRequisition  req;
-	gboolean        standalone = FALSE;
+	GtkRequisition	req;
+	gboolean	standalone = FALSE;
 	GtkSettings    *settings;
-	GError         *error = NULL;
-	GOptionEntry    entries[] = {
+	GError	       *error = NULL;
+	GOptionEntry	entries[] = {
 		{ "standalone", 's', 0, G_OPTION_ARG_NONE, &standalone,
 		_("Run in standalone mode"), NULL },
 		{ NULL }
@@ -127,12 +127,34 @@ main (int argc, char *argv[])
 		gtk_container_add (GTK_CONTAINER (window), content);
 		gtk_widget_show (window);
 
-                moblin_panel_shown (MOBLIN_PANEL (content));
+		moblin_panel_shown (MOBLIN_PANEL (content));
 	}  else {
+		GtkWidget *box, *label;
+		GdkScreen *screen;
+		char *s;
 		panel = mpl_panel_gtk_new (MPL_PANEL_BLUETOOTH, _("bluetooth"),
 					THEME_DIR "/bluetooth-panel.css",
 					"state-idle", TRUE);
-		window  = mpl_panel_gtk_get_window (MPL_PANEL_GTK (panel));
+		window	= mpl_panel_gtk_get_window (MPL_PANEL_GTK (panel));
+		box = gtk_vbox_new (FALSE, 0);
+		gtk_widget_show (box);
+		gtk_container_add (GTK_CONTAINER (window), box);
+
+		label = gtk_label_new (NULL);
+		screen = gdk_screen_get_default ();
+		s = g_strdup_printf ("<span foreground=\"#31c2ee\" weight=\"bold\" size=\"%d\">%s</span>",
+				     (int)(PANGO_SCALE * (22 * 72 / gdk_screen_get_resolution (screen))),
+				     _("Bluetooth"));
+		g_object_set (label,
+			      "label", s,
+			      "use-markup", TRUE,
+			      "xalign", 0.0f,
+			      "xpad", 16,
+			      "ypad", 8,
+			      NULL);
+		g_free (s);
+		gtk_widget_show (label);
+		gtk_box_pack_start (GTK_BOX (box), label, FALSE, FALSE, 0);
 
 		content = moblin_panel_new ();
 		g_signal_connect (panel, "show", (GCallback) panel_shown_cb, content);
@@ -142,8 +164,8 @@ main (int argc, char *argv[])
 		g_signal_connect (content, "request-focus",
 				  G_CALLBACK (panel_request_focus), panel);
 		gtk_widget_show (content);
+		gtk_box_pack_start (GTK_BOX (box), content, TRUE, TRUE, 0);
 
-		gtk_container_add (GTK_CONTAINER (window), content);
 		gtk_widget_size_request (window, &req);
 		mpl_panel_client_set_height_request (panel, req.height);
 	}
