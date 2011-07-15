e4ceaa852b837d0f52b391d75fdf1d786cceafba
Bastien Nocera <hadess@hadess.net>
2011-07-15 10:57:41 +0100

    properties: Work-around CC library disappearing
    I was asking for ridicule in the code dept.

--- properties/cc-bluetooth-panel.c
+++ properties/cc-bluetooth-panel.c
@@ -26,7 +26,7 @@
 #endif
 
 #include <glib/gi18n-lib.h>
-#include <libgnome-control-center/cc-shell.h>
+#include "cut-n-paste/cc-shell.h"
 
 #include "cc-bluetooth-panel.h"
 
--- properties/cc-bluetooth-panel.h
+++ properties/cc-bluetooth-panel.h
@@ -25,7 +25,7 @@
 #ifndef _CC_BLUETOOTH_PANEL_H
 #define _CC_BLUETOOTH_PANEL_H
 
-#include <libgnome-control-center/cc-panel.h>
+#include "cut-n-paste/cc-panel.h"
 
 G_BEGIN_DECLS
 
