de5884b350f6216c3d8d5bd0c7129b50dfb21482
Bastien Nocera <hadess@hadess.net>
2011-09-28 15:47:02 +0100

    lib: White space error

--- lib/bluetooth-client.c
+++ lib/bluetooth-client.c
@@ -962,7 +962,7 @@ _bluetooth_client_get_default_adapter_powered (BluetoothClient *self)
 	path = gtk_tree_row_reference_get_path (priv->default_adapter);
 	gtk_tree_model_get_iter (GTK_TREE_MODEL (priv->store), &iter, path);
 	gtk_tree_model_get (GTK_TREE_MODEL (priv->store), &iter, BLUETOOTH_COLUMN_POWERED, &ret, -1);
-	
+
 	return ret;
 }
 
