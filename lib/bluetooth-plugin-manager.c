cd2b42da84be3a6039d15f6e3aa1c83fd0695ae2
Daniele Forsi <dforsi@src.gnome.org>
2012-04-25 17:12:58 +0200

    lib: Add annotations for bluetooth_plugin_manager_get_widgets()
    Fixes:
    bluetooth-plugin-manager.c:137: Warning: GnomeBluetooth: bluetooth_plugin_manager_get_widgets: return value: Missing (element-type) annotation

--- lib/bluetooth-plugin-manager.c
+++ lib/bluetooth-plugin-manager.c
@@ -134,7 +134,7 @@ bluetooth_plugin_manager_cleanup (void)
  * Returns a list of widgets suitable for configuring the device
  * represented by @address, for the services listed in @uuids.
  *
- * Return value: a #GList of #GtkWidget, or %NULL is none.
+ * Return value: (element-type Gtk.Widget) (transfer none) (allow-null): a #GList of #GtkWidget, or %NULL if none.
  **/
 GList *
 bluetooth_plugin_manager_get_widgets (const char *bdaddr,
