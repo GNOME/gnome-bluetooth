GNOME/gnome-bluetooth

commit aabaada7ac0d24cf67590f7403156c68bc8f98d4

wizard: Make name match on substrings

wizard/pin.c
@@ -90,7 +90,9 @@ pin_db_parse_start_tag (GMarkupParseContext *ctx,
 			if (g_str_has_prefix (pdata->address, *attr_values) == FALSE)
 				return;
 		} else if (g_str_equal (*attr_names, "name")) {
-			if (g_strcmp0 (*attr_values, pdata->name) != 0)
+			if (*attr_values == NULL)
+				return;
+			if (strstr (*attr_values, pdata->name) == NULL)
 				return;
 		} else if (g_str_equal (*attr_names, "pin")) {
 			if (g_str_has_prefix (*attr_values, MAX_DIGITS_PIN_PREFIX) != FALSE) {

wizard/pin-code-database.xml
@@ -21,7 +21,8 @@
 
 <!--
   The wizard will match devices, following the order of the XML file [1],
-  on any combination of type, OUI, or name.
+  on any combination of type, OUI, or name. The name is matched through a
+  sub-string search (it should contain the name mentioned in this file if any).
 
   The special NULL pin means that the devices will not be paired, but
   connected to and marked as trusted. This is for devices such as mice

commit f1ede20e18c9b731d21e7ea8869c9a80933c11b2

wizard: Fix crash when doing substring-matching
Regression caused by aabaada7ac0d24cf67590f7403156c68bc8f98d4

https://bugzilla.redhat.com/show_bug.cgi?id=869083

wizard/pin.c
@@ -90,7 +90,7 @@ pin_db_parse_start_tag (GMarkupParseContext *ctx,
 			if (g_str_has_prefix (pdata->address, *attr_values) == FALSE)
 				return;
 		} else if (g_str_equal (*attr_names, "name")) {
-			if (*attr_values == NULL)
+			if (*attr_values == NULL || pdata->name == NULL)
 				return;
 			if (strstr (*attr_values, pdata->name) == NULL)
 				return;