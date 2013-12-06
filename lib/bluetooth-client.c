b8c14747bca0d7666718baecd11a192fc101a36b
Bastien Nocera <hadess@hadess.net>
2013-12-06 17:19:16 +0100

    lib: Downgrade warning to debug

--- lib/bluetooth-client.c
+++ lib/bluetooth-client.c
@@ -1393,9 +1393,9 @@ device_pair_callback (GDBusProxy         *proxy,
 	GError *error = NULL;
 
 	if (device1_call_pair_finish (DEVICE1(proxy), res, &error) == FALSE) {
-		g_warning ("Pair() failed for %s: %s",
-			   g_dbus_proxy_get_object_path (proxy),
-			   error->message);
+		g_debug ("Pair() failed for %s: %s",
+			 g_dbus_proxy_get_object_path (proxy),
+			 error->message);
 		g_simple_async_result_take_error (simple, error);
 	} else {
 		g_simple_async_result_set_op_res_gboolean (simple, TRUE);
