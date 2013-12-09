02f3636b369a9a20b0825a056be689ac5b6821cc
Bastien Nocera <hadess@hadess.net>
2013-12-10 00:38:43 +0100

    settings: More PIN testing

--- lib/test-pin.c
+++ lib/test-pin.c
@@ -7,9 +7,18 @@ int main (int argc, char **argv)
 	gboolean confirm;
 	char *pin;
 
+	g_setenv ("G_MESSAGES_DEBUG", "all", TRUE);
+
+#if 0
 	pin = get_pincode_for_device (BLUETOOTH_TYPE_KEYBOARD,
 				      "00:13:6C:00:00:00",
 				      "TomTom Remote",
+				      &max_digits,
+				      &confirm);
+#endif
+	pin = get_pincode_for_device (BLUETOOTH_TYPE_MOUSE,
+				      "0C:77:1A:00:00:00",
+				      "Some Apple mouse",
 				      &max_digits,
 				      &confirm);
 
