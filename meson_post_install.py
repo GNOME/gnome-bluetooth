b029fbe1ac0716163193a7e896a84e0f08bbcfdf
Iñigo Martínez <inigomartinez@gmail.com>
2017-11-14 09:59:24 +0100

    build: Rename build options
    Following the new meson porting guidelines, this patch renames the
    build options as follows:
    
    - Remove the 'enable' prefix from boolean options.
    - Remove the 'with' prefix from string options.
    - The character separator in multi-word options has been changed
      to underscore.
    
    https://bugzilla.gnome.org/show_bug.cgi?id=790332

--- meson_post_install.py
+++ meson_post_install.py
@@ -13,6 +13,6 @@ icon_dir = os.path.join(os.environ['MESON_INSTALL_PREFIX'], 'share', 'icons', 'h
 [os.rename(file, os.path.join(os.path.dirname(file), name_pattern.search(file).group(1)))
  for file in glob.glob(icon_dir + search_pattern, recursive=True)]
 
-if not os.environ.get('DESTDIR') and sys.argv[1] == 'icon-update':
+if not os.environ.get('DESTDIR') and sys.argv[1] == 'icon_update':
   print('Update icon cache...')
   subprocess.call(['gtk-update-icon-cache', '-f', '-t', icon_dir])
