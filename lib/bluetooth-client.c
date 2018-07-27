b27632c1f5dd0e32133625c252fe6b9d5a8a2493
Bastien Nocera <hadess@hadess.net>
2018-07-27 14:19:30 +0200

    client: Remove unused _bluetooth_client_get_discoverable()
    We stopped using it in the previous commit.

--- lib/bluetooth-client.c
+++ lib/bluetooth-client.c
@@ -944,36 +944,6 @@ _bluetooth_client_get_default_adapter_name (BluetoothClient *self)
 }
 
 /**
- * _bluetooth_client_get_discoverable:
- * @client: a #BluetoothClient
- *
- * Gets the default adapter's discoverable status, cached in the adapter model.
- *
- * Returns: the discoverable status, or FALSE if no default adapter exists
- */
-static gboolean
-_bluetooth_client_get_discoverable (BluetoothClient *client)
-{
-	BluetoothClientPrivate *priv;
-	GtkTreePath *path;
-	GtkTreeIter iter;
-	gboolean ret;
-
-	g_return_val_if_fail (BLUETOOTH_IS_CLIENT (client), FALSE);
-
-	priv = BLUETOOTH_CLIENT_GET_PRIVATE (client);
-	if (priv->default_adapter == NULL)
-		return FALSE;
-
-	path = gtk_tree_row_reference_get_path (priv->default_adapter);
-	gtk_tree_model_get_iter (GTK_TREE_MODEL (priv->store), &iter, path);
-	gtk_tree_model_get (GTK_TREE_MODEL (priv->store), &iter,
-                            BLUETOOTH_COLUMN_DISCOVERABLE, &ret, -1);
-
-	return ret;
-}
-
-/**
  * _bluetooth_client_set_discoverable:
  * @client: a #BluetoothClient object
  * @discoverable: whether the device should be discoverable
