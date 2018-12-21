0266bae09f1aea6a882e142019140e6230e3f989
Iñigo Martínez <inigomartinez@gmail.com>
2018-12-21 10:40:58 +0100

    build: Do not use prefix on directory variables
    Although usually directory variables are set by using the `prefix`
    directory, this might cause issues due to parameters that need
    relative directories.
    
    In order to ease the transition `prefix` directory has been stripped
    from directory variables and only has been appended when necessary.

--- meson_post_install.py
+++ meson_post_install.py
@@ -5,6 +5,8 @@ import subprocess
 import sys
 
 if not os.environ.get('DESTDIR') and sys.argv[1] == 'icon_update':
-  icondir = os.path.join(sys.argv[2], 'icons', 'hicolor')
+  prefix = os.environ['MESON_INSTALL_PREFIX']
+
+  icondir = os.path.join(prefix, sys.argv[2], 'icons', 'hicolor')
   print('Update icon cache...')
   subprocess.call(['gtk-update-icon-cache', '-f', '-t', icondir])
