61b3a0b17f21390308edc936532b386216a262e0
Bastien Nocera <hadess@hadess.net>
2021-02-10 17:25:38 +0100

    lib: Simplify BluetoothChooserCombo object declaration

--- lib/bluetooth-chooser-combo.h
+++ lib/bluetooth-chooser-combo.h
@@ -1,6 +1,6 @@
 /* -*- Mode: C; tab-width: 8; indent-tabs-mode: t; c-basic-offset: 8 -*- */
 /*
- * (C) Copyright 2007 Bastien Nocera <hadess@hadess.net>
+ * (C) Copyright 2007, 2021 Bastien Nocera <hadess@hadess.net>
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Library General Public
@@ -20,13 +20,8 @@
 
 #include <gtk/gtk.h>
 
-#define BLUETOOTH_TYPE_CHOOSER_COMBO     (bluetooth_chooser_combo_get_type ())
-#define BLUETOOTH_CHOOSER_COMBO(obj)     (G_TYPE_CHECK_INSTANCE_CAST ((obj), BLUETOOTH_TYPE_CHOOSER_COMBO, BluetoothChooserCombo))
-#define BLUETOOTH_IS_CHOOSER_COMBO(obj)  (G_TYPE_CHECK_INSTANCE_TYPE ((obj), BLUETOOTH_TYPE_CHOOSER_COMBO))
-#define BLUETOOTH_IS_CHOOSER_COMBO_CLASS(klass) (G_TYPE_CHECK_CLASS_TYPE((klass), \
-									 BLUETOOTH_TYPE_CHOOSER_COMBO))
-#define BLUETOOTH_GET_CHOOSER_COMBO_CLASS(obj) (G_TYPE_INSTANCE_GET_CLASS((obj), \
-				BLUETOOTH_TYPE_CHOOSER_COMBO, BluetoothChooserComboClass))
+G_DECLARE_FINAL_TYPE(BluetoothChooserCombo, bluetooth_chooser_combo, BLUETOOTH, CHOOSER_COMBO, GtkBox)
+#define BLUETOOTH_TYPE_CHOOSER_COMBO (bluetooth_chooser_combo_get_type ())
 
 /**
 * BLUETOOTH_CHOOSER_COMBO_FIRST_DEVICE:
@@ -34,27 +29,5 @@
 * A convenience value used to select the first device regardless of its address.
 **/
 #define BLUETOOTH_CHOOSER_COMBO_FIRST_DEVICE "00:00:00:00:00:00"
-
-typedef struct _BluetoothChooserComboPrivate BluetoothChooserComboPrivate;
-typedef struct _BluetoothChooserComboClass BluetoothChooserComboClass;
-
-/**
- * BluetoothChooserCombo:
- *
- * The <structname>BluetoothChooserCombo</structname> struct contains
- * only private fields and should not be directly accessed.
- */
-typedef struct _BluetoothChooserCombo {
-	GtkBox            parent;
-	BluetoothChooserComboPrivate *priv;
-} BluetoothChooserCombo;
-
-struct _BluetoothChooserComboClass {
-  GtkBoxClass parent_class;
-
-  void (*chooser_created) (BluetoothChooserCombo *self, GtkWidget *chooser);
-};
-
-GType		bluetooth_chooser_combo_get_type	(void);
 
 GtkWidget *	bluetooth_chooser_combo_new		(void);
