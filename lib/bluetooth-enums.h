GNOME/gnome-bluetooth

commit c9a513a6338f673bc26c2aa995f657048173be1e

lib: Add speaker type
Closes: #77

lib/bluetooth-enums.h
@@ -79,6 +79,7 @@ typedef enum {
  * @BLUETOOTH_TYPE_DISPLAY: a display
  * @BLUETOOTH_TYPE_WEARABLE: a wearable computer
  * @BLUETOOTH_TYPE_TOY: a toy or game
+ * @BLUETOOTH_TYPE_SPEAKERS: audio speaker or speakers
  *
  * The type of a Bluetooth device. See also %BLUETOOTH_TYPE_INPUT and %BLUETOOTH_TYPE_AUDIO
  **/
@@ -102,7 +103,8 @@ typedef enum {
 	BLUETOOTH_TYPE_SCANNER		= 1 << 16,
 	BLUETOOTH_TYPE_DISPLAY		= 1 << 17,
 	BLUETOOTH_TYPE_WEARABLE		= 1 << 18,
-	BLUETOOTH_TYPE_TOY		= 1 << 19
+	BLUETOOTH_TYPE_TOY		= 1 << 19,
+	BLUETOOTH_TYPE_SPEAKERS		= 1 << 20,
 } BluetoothType;
 
 #define _BLUETOOTH_TYPE_NUM_TYPES 20

lib/bluetooth-utils.c
@@ -185,6 +185,8 @@ bluetooth_class_to_type (guint32 class)
 		case 0x01:
 		case 0x02:
 			return BLUETOOTH_TYPE_HEADSET;
+		case 0x05:
+			return BLUETOOTH_TYPE_SPEAKERS;
 		case 0x06:
 			return BLUETOOTH_TYPE_HEADPHONES;
 		case 0x0b: /* VCR */

commit b652b4588bf0badf6422a13659472d12ba5b53be

lib: Update num-types macro, and audio bitmask
Fix BLUETOOTH_TYPE_AUDIO and _BLUETOOTH_TYPE_NUM_TYPES after speaker
type addition.

Fixes: c9a513a6338f673bc26c2aa995f657048173be1e

lib/bluetooth-enums.h
@@ -104,7 +104,7 @@ typedef enum {
 	BLUETOOTH_TYPE_SPEAKERS		= 1 << 20,
 } BluetoothType;
 
-#define _BLUETOOTH_TYPE_NUM_TYPES 20
+#define _BLUETOOTH_TYPE_NUM_TYPES 21
 
 /**
  * BLUETOOTH_TYPE_INPUT:
@@ -117,7 +117,7 @@ typedef enum {
  *
  * Use this value to select any Bluetooth audio device where a #BluetoothType enum is required.
  */
-#define BLUETOOTH_TYPE_AUDIO (BLUETOOTH_TYPE_HEADSET | BLUETOOTH_TYPE_HEADPHONES | BLUETOOTH_TYPE_OTHER_AUDIO)
+#define BLUETOOTH_TYPE_AUDIO (BLUETOOTH_TYPE_HEADSET | BLUETOOTH_TYPE_HEADPHONES | BLUETOOTH_TYPE_OTHER_AUDIO | BLUETOOTH_TYPE_SPEAKERS)
 
 /**
  * BluetoothColumn: