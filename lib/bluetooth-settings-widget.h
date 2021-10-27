c1a4e577ea546ac7a666f8df02c564802a589475
Georges Basile Stavracas Neto <georges.stavracas@gmail.com>
2021-10-27 12:10:48 -0300

    settings: Use G_DECLARE_DERIVABLE_TYPE
    Cleanup the header a bit.

--- lib/bluetooth-settings-widget.h
+++ lib/bluetooth-settings-widget.h
@@ -23,35 +23,12 @@
 #include <gtk/gtk.h>
 
 #define BLUETOOTH_TYPE_SETTINGS_WIDGET (bluetooth_settings_widget_get_type())
-#define BLUETOOTH_SETTINGS_WIDGET(obj) (G_TYPE_CHECK_INSTANCE_CAST((obj), \
-				BLUETOOTH_TYPE_SETTINGS_WIDGET, BluetoothSettingsWidget))
-#define BLUETOOTH_SETTINGS_WIDGET_CLASS(klass) (G_TYPE_CHECK_CLASS_CAST((klass), \
-				BLUETOOTH_TYPE_SETTINGS_WIDGET, BluetoothSettingsWidgetClass))
-#define BLUETOOTH_IS_SETTINGS_WIDGET(obj) (G_TYPE_CHECK_INSTANCE_TYPE((obj), \
-						BLUETOOTH_TYPE_SETTINGS_WIDGET))
-#define BLUETOOTH_IS_SETTINGS_WIDGET_CLASS(klass) (G_TYPE_CHECK_CLASS_TYPE((klass), \
-						BLUETOOTH_TYPE_SETTINGS_WIDGET))
-#define BLUETOOTH_GET_SETTINGS_WIDGET_CLASS(obj) (G_TYPE_INSTANCE_GET_CLASS((obj), \
-				BLUETOOTH_TYPE_SETTINGS_WIDGET, BluetoothSettingsWidgetClass))
+G_DECLARE_DERIVABLE_TYPE (BluetoothSettingsWidget, bluetooth_settings_widget, BLUETOOTH, SETTINGS_WIDGET, GtkBox)
 
-/**
- * BluetoothSettingsWidget:
- *
- * The <structname>BluetoothSettingsWidget</structname> struct contains
- * only private fields and should not be directly accessed.
- */
-typedef struct _BluetoothSettingsWidget BluetoothSettingsWidget;
-typedef struct _BluetoothSettingsWidgetClass BluetoothSettingsWidgetClass;
-
-struct _BluetoothSettingsWidget {
-	GtkBox parent;
+struct _BluetoothSettingsWidgetClass
+{
+  GtkBoxClass parent_class;
 };
-
-struct _BluetoothSettingsWidgetClass {
-	GtkBoxClass parent_class;
-};
-
-GType bluetooth_settings_widget_get_type (void);
 
 GtkWidget *bluetooth_settings_widget_new (void);
 
