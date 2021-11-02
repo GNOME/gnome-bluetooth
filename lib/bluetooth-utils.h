8d7e049d69677ce8274890eeb22878e09f5d20b7
Bastien Nocera <hadess@hadess.net>
2021-11-02 15:24:18 +0100

    lib: Remove unused bluetooth_type_to_filter_string()
    It was used to label the filter drop-down in the chooser widgets.

--- lib/bluetooth-utils.h
+++ lib/bluetooth-utils.h
@@ -60,7 +60,6 @@
 BluetoothType  bluetooth_class_to_type         (guint32 class);
 BluetoothType  bluetooth_appearance_to_type    (guint16 appearance);
 const gchar   *bluetooth_type_to_string        (guint type);
-const gchar   *bluetooth_type_to_filter_string (guint type);
 gboolean       bluetooth_verify_address        (const char *bdaddr);
 const char    *bluetooth_uuid_to_string        (const char *uuid);
 
