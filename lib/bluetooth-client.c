GNOME/gnome-bluetooth

commit 7956dd42630fd52ba30417fbd4a65894f5fb0d3c

lib: Fix default-adapter-* properties notifications
It doesn't make much sense for those properties to be sent only when the
default adapter gets powered, the API user should be told straight away.

This also fixes the "default-adapter-address" property never sending a
notification.

lib/bluetooth-client.c
@@ -495,14 +495,6 @@ adapter_notify_cb (Adapter1       *adapter,
 	} else if (g_strcmp0 (property, "discovering") == 0) {
 		g_object_notify (G_OBJECT (client), "default-adapter-setup-mode");
 	} else if (g_strcmp0 (property, "powered") == 0) {
-		gboolean powered = adapter1_get_powered (adapter);
-
-		if (powered) {
-			g_debug ("Default adapter is powered, so invalidating all the default-adapter* properties");
-			g_object_notify (G_OBJECT (client), "default-adapter");
-			g_object_notify (G_OBJECT (client), "default-adapter-setup-mode");
-			g_object_notify (G_OBJECT (client), "default-adapter-name");
-		}
 		g_object_notify (G_OBJECT (client), "default-adapter-powered");
 	}
 }
@@ -524,23 +516,16 @@ default_adapter_changed (GDBusObjectManager   *manager,
 
 	add_devices_to_list_store (client);
 
+	g_debug ("New default adapter so invalidating all the default-adapter* properties");
+	g_object_notify (G_OBJECT (client), "default-adapter");
+	g_object_notify (G_OBJECT (client), "default-adapter-address");
+	g_object_notify (G_OBJECT (client), "default-adapter-powered");
+	g_object_notify (G_OBJECT (client), "default-adapter-setup-mode");
+	g_object_notify (G_OBJECT (client), "default-adapter-name");
+
 	powered = adapter1_get_powered (client->default_adapter);
-
-	if (powered) {
-		g_debug ("New default adapter is powered, so invalidating all the default-adapter* properties");
-		g_object_notify (G_OBJECT (client), "default-adapter");
-		g_object_notify (G_OBJECT (client), "default-adapter-powered");
-		g_object_notify (G_OBJECT (client), "default-adapter-setup-mode");
-		g_object_notify (G_OBJECT (client), "default-adapter-name");
-		return;
-	}
-
-	/*
-	 * If the adapter is turn off (Powered = False in bluetooth) object
-	 * notifications will be sent only when a Powered = True signal arrives
-	 * from bluetoothd
-	 */
-	adapter_set_powered (client, adapter, TRUE);
+	if (powered)
+		adapter_set_powered (client, adapter, TRUE);
 }
 
 static void

commit ac439e23b8ca99d898933c8f8438b6149a42c9b1

lib: Fix adapter never getting powered after it's made default
Fix the inverted logic that meant that adapter_set_powered() was never
called when the adapter was unpowered.

Fixes: 7956dd42630fd52ba30417fbd4a65894f5fb0d3c

lib/bluetooth-client.c
@@ -534,7 +534,7 @@ default_adapter_changed (GDBusObjectManager   *manager,
 	g_object_notify (G_OBJECT (client), "default-adapter-name");
 
 	powered = adapter1_get_powered (client->default_adapter);
-	if (powered)
+	if (!powered)
 		adapter_set_powered (client, adapter, TRUE);
 }
 