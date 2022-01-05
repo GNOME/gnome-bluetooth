GNOME/gnome-bluetooth

commit 96b89f86e903cf9110957ec000261e015cf0c05a

lib: Add more debug when powering up the default adapter

lib/bluetooth-client.c
@@ -396,7 +396,8 @@ adapter_set_powered_cb (GDBusProxy *proxy,
 
 	ret = g_dbus_proxy_call_finish (proxy, res, &error);
 	if (!ret) {
-		g_debug ("Error setting property 'Powered' on interface org.bluez.Adapter1: %s (%s, %d)",
+		g_debug ("Error setting property 'Powered' on %s: %s (%s, %d)",
+			 g_dbus_proxy_get_object_path (proxy),
 			 error->message, g_quark_to_string (error->domain), error->code);
 	}
 }
@@ -419,6 +420,8 @@ adapter_set_powered (BluetoothClient *client,
 		return;
 	}
 
+	g_debug ("Powering up default adapter %s",
+		 g_dbus_proxy_get_object_path (G_DBUS_PROXY (client->default_adapter)));
 	variant = g_variant_new_boolean (powered);
 	g_dbus_proxy_call (G_DBUS_PROXY (client->default_adapter),
 			   "org.freedesktop.DBus.Properties.Set",

commit 71e60886629fbb132086605d66bae1a70962585f

lib: Correct debug statement in adapter_set_powered()
To take into account that we can power up, as well as down.

Fixes: 96b89f86

lib/bluetooth-client.c
@@ -420,7 +420,8 @@ adapter_set_powered (BluetoothClient *client,
 		return;
 	}
 
-	g_debug ("Powering up default adapter %s",
+	g_debug ("Powering %s default adapter %s",
+		 powered ? "up" : "down",
 		 g_dbus_proxy_get_object_path (G_DBUS_PROXY (client->default_adapter)));
 	variant = g_variant_new_boolean (powered);
 	g_dbus_proxy_call (G_DBUS_PROXY (client->default_adapter),