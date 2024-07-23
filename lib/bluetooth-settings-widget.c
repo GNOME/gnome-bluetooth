GNOME/gnome-bluetooth

commit eaf89ba9e0dc3004bf2e857dc2e39b33de767a32

settings: Wrap main widget inside a AdwStatusPage
This is the correct pattern to use for an icon with a title, a
subtitle, and widgets below it.

lib/bluetooth-settings-widget.c
@@ -1125,12 +1125,12 @@ update_properties (BluetoothSettingsWidget *self,
 	gtk_widget_set_visible (WID ("send_button"), FALSE);
 
 	/* Name */
-	gtk_window_set_title (GTK_WINDOW (self->properties_dialog), alias);
+	adw_status_page_set_title (ADW_STATUS_PAGE (WID ("status_page")), alias);
 	g_free (self->selected_name);
 	self->selected_name = alias;
 
 	/* Icon */
-	gtk_image_set_from_icon_name (GTK_IMAGE (WID ("image")), icon);
+	adw_status_page_set_icon_name (ADW_STATUS_PAGE (WID ("status_page")), icon);
 
 	/* Connection */
 	button = GTK_SWITCH (WID ("switch_connection"));

lib/settings.ui
@@ -26,129 +26,105 @@
           </object>
         </child>
         <child>
-          <object class="GtkScrolledWindow" id="properties_widget">
-            <property name="vexpand">True</property>
-            <property name="hexpand">True</property>
+          <object class="AdwStatusPage" id="status_page">
             <child>
-              <object class="AdwClamp">
-                <property name="maximum-size">450</property>
-                <property name="margin_start">12</property>
-                <property name="margin_end">12</property>
-                <property name="margin_top">12</property>
-                <property name="margin_bottom">36</property>
+              <object class="GtkBox">
+                <property name="orientation">vertical</property>
+                <property name="spacing">24</property>
+                <child>
+                  <object class="GtkListBox">
+                    <property name="selection-mode">none</property>
+                    <style>
+                      <class name="boxed-list"/>
+                    </style>
+                    <child>
+                      <object class="AdwActionRow">
+                        <property name="title" translatable="yes">Connection</property>
+                        <property name="activatable-widget">switch_connection</property>
+                        <child>
+                          <object class="GtkSpinner" id="connecting_spinner">
+                            <property name="visible">False</property>
+                            <property name="valign">center</property>
+                          </object>
+                        </child>
+                        <child>
+                          <object class="GtkSwitch" id="switch_connection">
+                            <property name="valign">center</property>
+                          </object>
+                        </child>
+                      </object>
+                    </child>
+                  </object>
+                </child>
+                <child>
+                  <object class="GtkListBox">
+                    <property name="selection-mode">none</property>
+                    <style>
+                      <class name="boxed-list"/>
+                    </style>
+                    <child>
+                      <object class="AdwActionRow" id="paired_row">
+                        <property name="title" translatable="yes">Paired</property>
+                        <property name="subtitle" translatable="yes">Yes</property>
+                        <style>
+                          <class name="property"/>
+                        </style>
+                      </object>
+                    </child>
+                    <child>
+                      <object class="AdwActionRow" id="type_row">
+                        <property name="title" translatable="yes">Type</property>
+                        <style>
+                          <class name="property"/>
+                        </style>
+                      </object>
+                    </child>
+                    <child>
+                      <object class="AdwActionRow" id="address_row">
+                        <property name="title" translatable="yes">Address</property>
+                        <property name="subtitle">00:00:00:00:00</property>
+                        <style>
+                          <class name="property"/>
+                        </style>
+                      </object>
+                    </child>
+                  </object>
+                </child>
                 <child>
                   <object class="GtkBox">
                     <property name="orientation">vertical</property>
-                    <property name="spacing">24</property>
+                    <property name="spacing">6</property>
                     <child>
-                      <object class="GtkImage" id="image">
-                        <property name="margin_top">16</property>
-                        <property name="margin_bottom">16</property>
-                        <property name="icon_name">image-missing</property>
-                        <property name="icon_size">large</property>
-                        <property name="use_fallback">True</property>
-                        <property name="pixel-size">128</property>
-                        <style>
-                          <class name="icon-dropshadow"/>
-                        </style>
+                      <object class="GtkButton" id="mouse_button">
+                        <property name="label" translatable="yes">_Mouse &amp; Touchpad Settings</property>
+                        <property name="use_underline">True</property>
                       </object>
                     </child>
                     <child>
-                      <object class="GtkListBox">
-                        <property name="selection-mode">none</property>
-                        <style>
-                          <class name="boxed-list"/>
-                        </style>
-                        <child>
-                          <object class="AdwActionRow">
-                            <property name="title" translatable="yes">Connection</property>
-                            <property name="activatable-widget">switch_connection</property>
-                            <child>
-                              <object class="GtkSpinner" id="connecting_spinner">
-                                <property name="visible">False</property>
-                                <property name="valign">center</property>
-                              </object>
-                            </child>
-                            <child>
-                              <object class="GtkSwitch" id="switch_connection">
-                                <property name="valign">center</property>
-                              </object>
-                            </child>
-                          </object>
-                        </child>
+                      <object class="GtkButton" id="sound_button">
+                        <property name="label" translatable="yes">_Sound Settings</property>
+                        <property name="use_underline">True</property>
                       </object>
                     </child>
                     <child>
-                      <object class="GtkListBox">
-                        <property name="selection-mode">none</property>
-                        <style>
-                          <class name="boxed-list"/>
-                        </style>
-                        <child>
-                          <object class="AdwActionRow" id="paired_row">
-                            <property name="title" translatable="yes">Paired</property>
-                            <property name="subtitle" translatable="yes">Yes</property>
-                            <style>
-                              <class name="property"/>
-                            </style>
-                          </object>
-                        </child>
-                        <child>
-                          <object class="AdwActionRow" id="type_row">
-                            <property name="title" translatable="yes">Type</property>
-                            <style>
-                              <class name="property"/>
-                            </style>
-                          </object>
-                        </child>
-                        <child>
-                          <object class="AdwActionRow" id="address_row">
-                            <property name="title" translatable="yes">Address</property>
-                            <property name="subtitle">00:00:00:00:00</property>
-                            <style>
-                              <class name="property"/>
-                            </style>
-                          </object>
-                        </child>
+                      <object class="GtkButton" id="keyboard_button">
+                        <property name="label" translatable="yes">_Keyboard Settings</property>
+                        <property name="use_underline">True</property>
                       </object>
                     </child>
                     <child>
-                      <object class="GtkBox">
-                        <property name="orientation">vertical</property>
-                        <property name="spacing">6</property>
-                        <child>
-                          <object class="GtkButton" id="mouse_button">
-                            <property name="label" translatable="yes">_Mouse &amp; Touchpad Settings</property>
-                            <property name="use_underline">True</property>
-                          </object>
-                        </child>
-                        <child>
-                          <object class="GtkButton" id="sound_button">
-                            <property name="label" translatable="yes">_Sound Settings</property>
-                            <property name="use_underline">True</property>
-                          </object>
-                        </child>
-                        <child>
-                          <object class="GtkButton" id="keyboard_button">
-                            <property name="label" translatable="yes">_Keyboard Settings</property>
-                            <property name="use_underline">True</property>
-                          </object>
-                        </child>
-                        <child>
-                          <object class="GtkButton" id="send_button">
-                            <property name="label" translatable="yes">Send _Files…</property>
-                            <property name="use_underline">True</property>
-                          </object>
-                        </child>
-                        <child>
-                          <object class="GtkButton" id="delete_button">
-                            <property name="label" translatable="yes">_Remove Device</property>
-                            <property name="use-underline">True</property>
-                            <style>
-                              <class name="destructive-action"/>
-                            </style>
-                          </object>
-                        </child>
+                      <object class="GtkButton" id="send_button">
+                        <property name="label" translatable="yes">Send _Files…</property>
+                        <property name="use-underline">True</property>
+                      </object>
+                    </child>
+                    <child>
+                      <object class="GtkButton" id="delete_button">
+                        <property name="label" translatable="yes">_Remove Device</property>
+                        <property name="use-underline">True</property>
+                        <style>
+                          <class name="destructive-action"/>
+                        </style>
                       </object>
                     </child>
                   </object>

commit 32db13cfe4924ddb6b300708dbe77454f2ded9bd

settings: Fix symbolic icons showing in device properties
Use icon name fallback to set a very precise icon name, that will
automatically fall back to a less precise one if not available.

This fixes the phone icons in the Bluetooth Settings appearing as
generic symbolic icons rather than colourful ones.

Fixes: eaf89ba9e0dc ("settings: Wrap main widget inside a AdwStatusPage")
Closes: #150

lib/bluetooth-settings-widget.c
@@ -1091,6 +1091,32 @@ switch_connected_state_set (GtkSwitch               *button,
 }
 
 static void
+apply_use_fallback (GtkWidget *widget)
+{
+	GtkWidget *child;
+
+	for (child = gtk_widget_get_first_child (widget);
+	     child != NULL;
+	     child = gtk_widget_get_next_sibling (child)) {
+		if (GTK_IS_IMAGE (child)) {
+			g_object_set (G_OBJECT (child), "use-fallback", TRUE, NULL);
+			break;
+		} else {
+			apply_use_fallback (child);
+		}
+	}
+
+}
+
+static void
+status_page_set_icon_name (GtkWidget  *status_page,
+			   const char *icon_name)
+{
+	adw_status_page_set_icon_name (ADW_STATUS_PAGE (status_page), icon_name);
+	apply_use_fallback (status_page);
+}
+
+static void
 update_properties (BluetoothSettingsWidget *self,
 		   BluetoothDevice         *device)
 {
@@ -1130,7 +1156,7 @@ update_properties (BluetoothSettingsWidget *self,
 	self->selected_name = alias;
 
 	/* Icon */
-	adw_status_page_set_icon_name (ADW_STATUS_PAGE (WID ("status_page")), icon);
+	status_page_set_icon_name (WID ("status_page"), icon);
 
 	/* Connection */
 	button = GTK_SWITCH (WID ("switch_connection"));