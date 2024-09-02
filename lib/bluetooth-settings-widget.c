GNOME/gnome-bluetooth

commit dbff884e8e57e2e4e931a7a791134cc96d2c052f

settings: Don't use AdwStatusPage as a container of the page widgets
AdwStatusPage isn't mean to act like a whole container for the page; this
can break scrolling as AdwStatusPage contains a GtkScrolledWindow. Also,
there is already AdwPreferencesPage, which is the proper widget to use in
this case.

Use AdwPreferencesPage and AdwPreferencesGroup instead.

lib/bluetooth-settings-widget.c
@@ -1121,12 +1121,12 @@ update_properties (BluetoothSettingsWidget *self,
 	gtk_widget_set_visible (WID ("send_button"), FALSE);
 
 	/* Name */
-	adw_status_page_set_title (ADW_STATUS_PAGE (WID ("status_page")), alias);
+        gtk_label_set_label (GTK_LABEL (WID ("device_name")), alias);
 	g_free (self->selected_name);
 	self->selected_name = alias;
 
 	/* Icon */
-	adw_status_page_set_icon_name (ADW_STATUS_PAGE (WID ("status_page")), icon);
+        gtk_image_set_from_icon_name (GTK_IMAGE (WID ("device_icon")), icon);
 
 	/* Connection */
 	button = GTK_SWITCH (WID ("switch_connection"));

lib/settings.ui
@@ -25,73 +25,92 @@
             <property name="show-title">False</property>
           </object>
         </child>
-        <child>
-          <object class="AdwStatusPage" id="status_page">
+        <property name="content">
+          <object class="AdwPreferencesPage">
             <child>
-              <object class="GtkBox">
-                <property name="orientation">vertical</property>
-                <property name="spacing">24</property>
+              <object class="AdwPreferencesGroup">
                 <child>
-                  <object class="GtkListBox">
-                    <property name="selection-mode">none</property>
-                    <style>
-                      <class name="boxed-list"/>
-                    </style>
+                  <object class="GtkBox">
+                    <property name="orientation">vertical</property>
+                    <property name="spacing">12</property>
                     <child>
-                      <object class="AdwActionRow">
-                        <property name="title" translatable="yes">_Connection</property>
-                        <property name="activatable-widget">switch_connection</property>
-                        <property name="use-underline">True</property>
-                        <child>
-                          <object class="GtkSpinner" id="connecting_spinner">
-                            <property name="visible">False</property>
-                            <property name="valign">center</property>
-                          </object>
-                        </child>
-                        <child>
-                          <object class="GtkSwitch" id="switch_connection">
-                            <property name="valign">center</property>
-                          </object>
-                        </child>
-                      </object>
-                    </child>
-                  </object>
-                </child>
-                <child>
-                  <object class="GtkListBox">
-                    <property name="selection-mode">none</property>
-                    <style>
-                      <class name="boxed-list"/>
-                    </style>
-                    <child>
-                      <object class="AdwActionRow" id="paired_row">
-                        <property name="title" translatable="yes">Paired</property>
-                        <property name="subtitle" translatable="yes">Yes</property>
+                      <object class="GtkImage" id="device_icon">
+                        <property name="pixel-size">96</property>
                         <style>
-                          <class name="property"/>
+                          <class name="icon-dropshadow"/>
                         </style>
                       </object>
                     </child>
                     <child>
-                      <object class="AdwActionRow" id="type_row">
-                        <property name="title" translatable="yes">Type</property>
+                      <object class="GtkLabel" id="device_name">
+                        <property name="wrap">True</property>
+                        <property name="wrap-mode">word-char</property>
+                        <property name="justify">center</property>
                         <style>
-                          <class name="property"/>
-                        </style>
-                      </object>
-                    </child>
-                    <child>
-                      <object class="AdwActionRow" id="address_row">
-                        <property name="title" translatable="yes">Address</property>
-                        <property name="subtitle">00:00:00:00:00</property>
-                        <property name="subtitle-selectable">True</property>
-                        <style>
-                          <class name="property"/>
+                          <class name="title"/>
+                          <class name="title-1"/>
                         </style>
                       </object>
                     </child>
                   </object>
                 </child>
+              </object>
+            </child>
+            <child>
+              <object class="AdwPreferencesGroup">
+                <child>
+                  <object class="AdwActionRow">
+                    <property name="title" translatable="yes">_Connection</property>
+                    <property name="activatable-widget">switch_connection</property>
+                    <property name="use-underline">True</property>
+                    <child>
+                      <object class="GtkSpinner" id="connecting_spinner">
+                        <property name="visible">False</property>
+                        <property name="valign">center</property>
+                      </object>
+                    </child>
+                    <child>
+                      <object class="GtkSwitch" id="switch_connection">
+                        <property name="valign">center</property>
+                      </object>
+                    </child>
+                  </object>
+                </child>
+              </object>
+            </child>
+            <child>
+              <object class="AdwPreferencesGroup">
+                <child>
+                  <object class="AdwActionRow" id="paired_row">
+                    <property name="title" translatable="yes">Paired</property>
+                    <property name="subtitle" translatable="yes">Yes</property>
+                    <style>
+                      <class name="property"/>
+                    </style>
+                  </object>
+                </child>
+                <child>
+                  <object class="AdwActionRow" id="type_row">
+                    <property name="title" translatable="yes">Type</property>
+                    <style>
+                      <class name="property"/>
+                    </style>
+                  </object>
+                </child>
+                <child>
+                  <object class="AdwActionRow" id="address_row">
+                    <property name="title" translatable="yes">Address</property>
+                    <property name="subtitle">00:00:00:00:00</property>
+                    <property name="subtitle-selectable">True</property>
+                    <style>
+                      <class name="property"/>
+                    </style>
+                  </object>
+                </child>
+              </object>
+            </child>
+            <child>
+              <object class="AdwPreferencesGroup">
                 <child>
                   <object class="GtkBox">
                     <property name="orientation">vertical</property>
@@ -134,7 +153,7 @@
               </object>
             </child>
           </object>
-        </child>
+        </property>
       </object>
     </child>
   </object>

commit 19878d31f8ab39d81eeddc904e34e751d1d38547

bluetooth-settings-widget: Add a title to the device properties dialog
Doing so, Orca will properly announce the dialog instead of just
saying "dialog". With this change, Orca will say "[device name]
dialog".

This fixes a regression introduced in https://gitlab.gnome.org/GNOME/gnome-bluetooth/-/commit/dbff884e8e57e2e4e931a7a791134cc96d2c052f.
Previously, the device name was shown as an AdwStatusPage:title,
which was announced by Orca.

lib/bluetooth-settings-widget.c
@@ -1113,6 +1113,9 @@ update_properties (BluetoothSettingsWidget *self,
 	gtk_widget_set_visible (WID ("mouse_button_row"), FALSE);
 	gtk_widget_set_visible (WID ("send_button_row"), FALSE);
 
+        /* Properties dialog title */
+        adw_dialog_set_title (ADW_DIALOG (WID ("properties_dialog")), alias);
+
 	/* Name */
         gtk_label_set_label (GTK_LABEL (WID ("device_name")), alias);
 	g_free (self->selected_name);