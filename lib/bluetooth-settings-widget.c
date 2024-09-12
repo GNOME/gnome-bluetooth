GNOME/gnome-bluetooth

commit ba8bd8bd1f5823e5af1741bd089abeed4e309302

settings: Show a placeholder when the devices list is empty
When there are no devices, the list doesn't display
the "Searching for devices..." placeholder. Instead,
a broken GtkListBox is shown.

Fix that by using a GtkListBox placeholder to show the
"Searching for devices..." message.

Closes https://gitlab.gnome.org/GNOME/gnome-bluetooth/-/issues/113

lib/bluetooth-settings-widget.c
@@ -63,7 +63,6 @@ struct _BluetoothSettingsWidget {
 	GtkWidget           *device_list;
 	GtkAdjustment       *focus_adjustment;
 	GtkSizeGroup        *row_sizegroup;
-	GtkWidget           *device_stack;
 	GtkWidget           *device_spinner;
 	GHashTable          *connecting_devices; /* key=bdaddr, value=boolean */
 
@@ -104,9 +103,6 @@ static guint signals[LAST_SIGNAL] = { 0 };
 #define GNOME_SESSION_DBUS_NAME      "org.gnome.SessionManager"
 #define GNOME_SESSION_DBUS_OBJECT    "/org/gnome/SessionManager"
 #define GNOME_SESSION_DBUS_INTERFACE "org.gnome.SessionManager"
-
-#define FILLER_PAGE                  "filler-page"
-#define DEVICES_PAGE                 "devices-page"
 
 enum {
 	CONNECTING_STATE_IDLE = 0,
@@ -1465,8 +1461,6 @@ add_device_section (BluetoothSettingsWidget *self)
 				    (GtkListBoxSortFunc)device_sort_func, NULL, NULL);
 	g_signal_connect_swapped (self->device_list, "row-activated",
 				  G_CALLBACK (activate_row), self);
-
-	self->device_stack = WID ("device_stack");
 }
 
 static void
@@ -1535,10 +1529,6 @@ device_added_cb (BluetoothClient *client,
 	gtk_list_box_append (GTK_LIST_BOX (self->device_list), row);
 	gtk_size_group_add_widget (self->row_sizegroup, row);
 
-	gtk_stack_set_transition_type (GTK_STACK (self->device_stack),
-				       GTK_STACK_TRANSITION_TYPE_SLIDE_DOWN);
-	gtk_stack_set_visible_child_name (GTK_STACK (self->device_stack), DEVICES_PAGE);
-
 	g_signal_connect_object (G_OBJECT (device), "notify",
 				 G_CALLBACK (device_changed_cb), self, 0);
 }
@@ -1573,15 +1563,9 @@ device_removed_cb (BluetoothClient *client,
 		}
 	}
 
-	if (found) {
-		if (gtk_widget_get_first_child (self->device_list) == NULL) {
-			gtk_stack_set_transition_type (GTK_STACK (self->device_stack),
-						       GTK_STACK_TRANSITION_TYPE_NONE);
-			gtk_stack_set_visible_child_name (GTK_STACK (self->device_stack), FILLER_PAGE);
-		}
-	} else {
-		g_debug ("Didn't find a row to remove for tree path %s", object_path);
-	}
+        if (!found) {
+             g_debug ("Didn't find a row to remove for tree path %s", object_path);
+        }
 }
 
 static void

lib/settings.ui
@@ -146,38 +146,28 @@
           <object class="GtkSpinner" id="device_spinner"/>
         </property>
         <child>
-          <object class="GtkStack" id="device_stack">
-            <property name="hhomogeneous">False</property>
-            <property name="vhomogeneous">False</property>
-            <child>
-              <object class="GtkStackPage">
-                <property name="name">devices-page</property>
-                <property name="child">
-                  <object class="GtkListBox" id="device_list">
-                    <property name="selection-mode">none</property>
-                    <accessibility>
-                      <property name="label" translatable="yes">Devices</property>
-                    </accessibility>
-                    <style>
-                      <class name="boxed-list"/>
-                    </style>
-                  </object>
-                </property>
+          <object class="GtkListBox" id="device_list">
+            <property name="selection-mode">none</property>
+            <child type="placeholder">
+              <object class="GtkLabel">
+                <property name="margin-start">18</property>
+                <property name="margin-end">18</property>
+                <property name="margin-top">18</property>
+                <property name="margin-bottom">18</property>
+                <property name="label" translatable="yes">Searching for devices…</property>
+                <property name="wrap">true</property>
+                <property name="max-width-chars">50</property>
+                <style>
+                  <class name="dim-label" />
+                </style>
               </object>
             </child>
-            <child>
-              <object class="GtkStackPage">
-                <property name="name">filler-page</property>
-                <property name="child">
-                  <object class="GtkLabel">
-                    <property name="label" translatable="yes">Searching for devices…</property>
-                    <style>
-                      <class name="dim-label"/>
-                    </style>
-                  </object>
-                </property>
-              </object>
-            </child>
+            <accessibility>
+              <property name="label" translatable="yes">Devices</property>
+            </accessibility>
+            <style>
+              <class name="boxed-list"/>
+            </style>
           </object>
         </child>
       </object>

commit 5aed7ac8f3390b3c65d5277cb1000ce535d9d1fe

settings-widget: Fix crash when pairing is cancelled
When a pairing gets cancelled, we would try and turn off pairing for the
devices in the device list, but the first child we'd hit isn't a row
representing a device, but the placeholder for when there aren't any
devices in that list.

GLib-GObject[16799]:CRITICAL: g_object_set_is_valid_property: object class 'GtkLabel' has no property named 'pairing'

Fixes: ba8bd8bd1f58 ("settings: Show a placeholder when the devices list is empty")
Closes: #156

lib/bluetooth-settings-widget.c
@@ -651,7 +651,8 @@ cancel_callback (GDBusMethodInvocation *invocation,
 
 	child = gtk_widget_get_first_child (self->device_list);
 	while (child) {
-		g_object_set (G_OBJECT (child), "pairing", FALSE, NULL);
+		if (GTK_IS_LIST_BOX_ROW (child))
+			g_object_set (G_OBJECT (child), "pairing", FALSE, NULL);
 		child = gtk_widget_get_next_sibling (child);
 	}
 